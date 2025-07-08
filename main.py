08815a9fe0de2025f08b10de2d8a81c1e0dea041
Daniel <abodaniel031006@gmail.com>
2025-07-08 21:35:08 +0200

    8

--- main.py
+++ main.py
@@ -15,23 +15,11 @@ app.add_middleware(
 # POST-запрос на /check
 @app.post("/check")
 async def check_chat_id(request: Request):
-    # Получаем JSON из запроса
+    
     data = await request.json()
     chat_id = data.get("chat_id")
 
-    if not chat_id:
-        return {"message": "chat_id не передан"}
-
-    # Подключаемся к базе данных spy.db
-    async with aiosqlite.connect("spy.db") as db:
-        # Проверяем, есть ли такой chat_id в таблице spy_messages
-        cursor = await db.execute("SELECT 1 FROM spy_messages WHERE chat_id = ? LIMIT 1", (chat_id,))
-        result = await cursor.fetchone()
-
-        # Выводим в лог (Render покажет в логах)
-        if result:
-            print(f"✅ chat_id {chat_id} найден в базе")
-            return {"message": f"ID {chat_id} найден ✅"}
-        else:
-            print(f"❌ chat_id {chat_id} НЕ найден в базе")
-            return {"message": f"ID {chat_id} не найден ❌"}
+    if chat_id == 640055320:
+        return {"message": f"ID {chat_id} найден ✅"}
+    else:
+        return {"message": f"ID {chat_id} не найден ❌"}
